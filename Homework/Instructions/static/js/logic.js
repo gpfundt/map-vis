f56a16fb69c3109f39b6353fbc33ae5f0acace15
Greta Pfundt <g.pfundt325@gmail.com>
2020-01-17 20:15:37 -0500

    earthquakes

---
+++ Homework/Instructions/static/js/logic.js
@@ -0,0 +1,43 @@
+// Create a map using Leaflet that plots all of the earthquakes from your data set 
+// based on their longitude and latitude.
+
+// * Your data markers should reflect the magnitude of the earthquake in their size 
+// and color. Earthquakes with higher magnitudes should appear larger and darker in 
+// color.
+
+// * Include popups that provide additional information about the earthquake when a 
+// marker is clicked.
+
+// * Create a legend that will provide context for your map data.
+
+// * Your visualization should look something like the map above.
+
+
+url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
+API_KEY = "pk.eyJ1IjoiZ3BmdW5kdDMiLCJhIjoiY2s1aXc5aDUzMGo0ODNrbXNndnlyZHFtNyJ9.YWGks_INxxA82dgB6gRLoA";
+
+
+var myMap = L.map('map', {
+    center: [34, 30],
+    zoom: 2
+});
+
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+d3.json(url, function(data){
+    console.log(data)
+
+    L.geoJSON(data, {
+        style: function (feature) {
+            return {color: feature.properties.mag};
+        }
+    }).bindPopup(function (layer) {
+        return layer.feature.properties.mag;
+    }).addTo(myMap);
+
+});
